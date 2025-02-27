bef43af685b68210fc0be3e63d0d420f4edd3be0
jnewell-7 <jacksonnewell07@gmail.com>
2025-02-26 21:56:15 -0700

    Added first few things on the controller and created quadrants model/viewbag

--- Mission08_Team0105/Models/Task.cs
+++ Mission08_Team0105/Models/Task.cs
@@ -7,15 +7,21 @@ namespace Mission08_Team0105.Models
     {
         [Key]
         public int TaskId { get; set; }
+        
         [Required]
         public string TaskName { get; set; } 
+        
         public string? DueDate { get; set; }
+        
+        public int QuadrantId { get; set; }
+        public int QuadrantName { get; set; }
+        
         public bool? Completed { get; set; }
 
         [ForeignKey("CategoryID")]
-        public virtual Category Category { get; set; }
-
-        public int? CategoryID { get; set; }
+        public int CategoryID { get; set; }
+        
+        public virtual Category? Category { get; set; }
 
     }
 }
