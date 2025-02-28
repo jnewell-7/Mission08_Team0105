1f31d416df6fd9030ea2c76859f2a70e2c0663ba
jnewell-7 <jacksonnewell07@gmail.com>
2025-02-28 15:34:05 -0700

    installed tools

--- Mission08_Team0105/obj/Debug/net8.0/Mission08_Team0105.AssemblyInfo.cs
+++ Mission08_Team0105/obj/Debug/net8.0/Mission08_Team0105.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission08_Team0105")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9c6e73124e1354353a3eefc566b9edc7d057007")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd279a0bb77583599a483e95d48a6ccb89ffafe7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission08_Team0105")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission08_Team0105")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
