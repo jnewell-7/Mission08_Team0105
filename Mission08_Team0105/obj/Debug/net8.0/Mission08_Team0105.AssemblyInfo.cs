d87f93b89dc20dd2fe14a047692284304ca6b39c
jnewell-7 <jacksonnewell07@gmail.com>
2025-02-28 17:15:43 -0700

    updates to controllers and addition of confirmation and delete pages

--- Mission08_Team0105/obj/Debug/net8.0/Mission08_Team0105.AssemblyInfo.cs
+++ Mission08_Team0105/obj/Debug/net8.0/Mission08_Team0105.AssemblyInfo.cs
@@ -13,9 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission08_Team0105")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18c3da33eb8a16b03dd953b2ce675c39807e4d94")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd279a0bb77583599a483e95d48a6ccb89ffafe7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78a5dbba6bfc9b2e9ac9f843b3094d9c2476d215")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission08_Team0105")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission08_Team0105")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
